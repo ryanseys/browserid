3b4381df6088b96cba28088469dfc24023305878
Ryan Seys <ryan@ryanseys.com>
2013-06-20 14:59:14 -0700

    Tests for rtcweb://peerconnection uri

--- tests/verifier-test.js
+++ tests/verifier-test.js
@@ -76,7 +76,8 @@ suite.addBatch({
     'http://fakesite.com:80 and http://fakesite.com:8000': matchesAudience(false),
     'https://fakesite.com:443 and https://fakesite.com:9000': matchesAudience(false),
 
-    'app://browser.gaiamobile.org and app://browser.gaiamobile.org:80': matchesAudience(true)
+    'app://browser.gaiamobile.org and app://browser.gaiamobile.org:80': matchesAudience(true),
+    'rtcweb://peerconnection and rtcweb://peerconnection': matchesAudience(true)
   }
 });
 
